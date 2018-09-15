31cb434550813f83fcfe00e07eca44bb716f124f
Marko Stankovic <stankvic_marko@hotmail.com>
2018-09-15 12:37:34 +0200

    Add css

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<title>Login - test</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="css/stil.css">
+	<link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
